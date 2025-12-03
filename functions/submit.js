62d1b4e29ead583a5b03d9f461a8cb217c480f4b
Ryan4012 <246ryan@gmail.com>
2025-12-03 14:26:20 -0800

    contact form adjustments

---
+++ functions/submit.js
@@ -0,0 +1,25 @@
+export async function onRequest(context) {
+    try {
+      const formData = await context.request.formData();
+
+      formData.append('access_key', context.env.access_key);
+  
+      const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        body: formData
+      });
+  
+      const result = await response.json();
+  
+      return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' },
+        status: response.status,
+      });
+    } catch (error) {
+      return new Response(JSON.stringify({ success: false, error: error.message }), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 500,
+      });
+    }
+  }
+  
